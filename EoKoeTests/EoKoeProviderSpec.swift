ffb532a7d50baf73047fd47ac1fc18abb87cca0e
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-02 05:33:38 -0300

    Create Api specs file for test

--- EoKoeTests/EoKoeProviderSpec.swift
+++ EoKoeTests/EoKoeProviderSpec.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2018 Jaime Costa Marques. All rights reserved.
 //
 
+
+import Quick
+import Nimble
 import Foundation
+
+@testable import EoKoe
+
+final class EoKoeProviderSpec: QuickSpec {
+    override func spec() {
+
+        it("returns stubbed data for users list request") {
+            var message: String?
+        }
+    }
+}
