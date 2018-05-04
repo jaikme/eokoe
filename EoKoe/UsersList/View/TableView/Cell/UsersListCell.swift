5e753f6cc0c4ac7b95af65c51a5895799de35770
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-04 03:40:08 -0300

    Create initial userList table cell class

---
+++ EoKoe/UsersList/View/TableView/Cell/UsersListCell.swift
@@ -0,0 +1,17 @@
+//
+//  UsersListCell.swift
+//  EoKoe
+//
+//  Created by Jaime Costa Marques on 04/05/18.
+//  Copyright © 2018 Jaime Costa Marques. All rights reserved.
+//
+
+import UIKit
+
+final class UsersListCell : UITableViewCell {
+
+    @IBOutlet private weak var userIMage: UIImageView!
+    @IBOutlet private weak var userName: UILabel!
+    @IBOutlet private weak var userBio: UILabel!
+    
+}
