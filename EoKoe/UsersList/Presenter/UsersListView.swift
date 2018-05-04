25278d09ba77d26bca7fd52c760f43e773aefeb9
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-04 03:39:42 -0300

    Create user list view protocol

---
+++ EoKoe/UsersList/Presenter/UsersListView.swift
@@ -0,0 +1,13 @@
+//
+//  UsersListView.swift
+//  EoKoe
+//
+//  Created by Jaime Costa Marques on 04/05/18.
+//  Copyright © 2018 Jaime Costa Marques. All rights reserved.
+//
+
+import Foundation
+
+public protocol UsersListView : class {
+
+}
