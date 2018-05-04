587c80155ec0e6ffcf2b4b56b308f0a2b99de57c
Jaime C Marques <jaime.mqu@gmail.com>
2018-05-03 22:15:09 -0300

    Create a JSON helper

---
+++ EoKoe/Common/JSON.swift
@@ -0,0 +1,11 @@
+//
+//  JSON.swift
+//  EoKoe
+//
+//  Created by Jaime Costa Marques on 03/05/18.
+//  Copyright © 2018 Jaime Costa Marques. All rights reserved.
+//
+
+import Foundation
+
+public typealias JSON = [String: Any]
