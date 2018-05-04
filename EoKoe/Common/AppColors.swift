9f3f8015d55ac7e682f85b6cc31ac2ab01f7fee1
Jaime Costa Marques <jmarques@mundipagg.com>
2018-05-04 01:27:25 -0300

    Create app colors constants to fix color precision issues

---
+++ EoKoe/Common/AppColors.swift
@@ -0,0 +1,18 @@
+//
+//  AppColors.swift
+//  EoKoe
+//
+//  Created by Jaime Costa Marques on 04/05/18.
+//  Copyright © 2018 Jaime Costa Marques. All rights reserved.
+//
+
+import UIKit
+
+public struct AppColors {
+    
+    private init() { }
+    
+    /// #F65669
+    public static let usersListNavbarColor = #colorLiteral(red: 0.9647058824, green: 0.337254902, blue: 0.4117647059, alpha: 1)
+    
+}
